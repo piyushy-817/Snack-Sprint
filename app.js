a40358d4c96318aa0b2546386ae64447c1a04b3c
Piyush Yadav <piyushyadav970@gmail.com>
2024-10-19 14:11:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const mamle = React.createElement(
+  "h2",
+  { id: "tight", klklkl: "aoop" },
+  [React.createElement("div", { id: "tight1", klklkl: "aoop" }, "HIii jkfkbjfb"),
+  React.createElement("div", { id: "tight2", klklkl: "aoop" }, "HIii jkfb"),
+  React.createElement("div", { id: "tight3", klklkl: "aoop" }, "HIii jkfb")])
+
+
+const rooting = ReactDOM.createRoot(document.getElementById("root"));
+console.log(mamle);
+rooting.render(mamle);
+console.log(rooting);
